7912a35d82df92e5ede6d9ed1347e5cde415867d
Caique Brunholi do Nascimento <caiquebn@gmail.com>
2022-11-23 19:06:53 -0300

    Dia 01 de JS

---
+++ fundamentos/secao-04-introducao-javascript/dia-01/index.js
@@ -0,0 +1,8 @@
+const myName = "Caique";
+const birthCity = "Birigui";
+let birthYear = 1994;
+birthYear  = 2030;
+
+console.log (myName)
+console.log (birthCity)
+console.log (birthYear)
---
+++ fundamentos/secao-04-introducao-javascript/exercicios.js
@@ -0,0 +1,51 @@
+const a = 8;
+const b = 8;
+
+let adicao = a + b;
+let subtracao = a - b;
+let multiplicacao = a * b;
+let divisao = a / b;
+let modulo = a % b;
+
+console.log(adicao);
+console.log(subtracao);
+console.log(multiplicacao);
+console .log(divisao);
+console.log(modulo);
+
+const c = 11;
+const d = 13;
+
+if (c > d) {
+    console.log("c é maior que d");
+} else {
+    console.log("d é menor que c");
+};
+
+const e = 7;
+const f = 8;
+const g = 9;
+
+if (e > f && e < g) {
+    console.log("O maior numero é E");
+} else if (f > g && f > g) {
+    console.log("O maior numero é F");
+} else {
+    console.log("O maior numero é G");
+};
+
+const h = 0;
+
+if (h > 0) {
+    console.log("positive");
+} else if (h < 0) {
+    console.log("negative");
+} else {
+    console.log("zero");
+};
+
+const trianguloA = 0;
+const trianguloB = 8;
+const trianguloC = 10;
+
+let somaTriangulo = trianguloA + trianguloB + trianguloC
